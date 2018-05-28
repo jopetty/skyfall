f6d843f0bae627a0262765159463153bb8d58193
Jackson Petty <jacksonopetty@gmail.com>
2018-05-28 12:29:24 -0600

    Begin calculating projection data

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,89 @@ extern crate time;
 
 mod record;
 
-use record::{Position, Star, StellarPosition};
+use record::{Position, Star, PolarPosition};
 use std::error::Error;
-use std::process;
-use time::Tm;
+// use std::process;
+// use time::Tm;
 
-fn get_position(star: Star, position: Position, time: Tm) -> StellarPosition {
-    let right_ascension = star.right_ascension;
-    let declination = star.declination;
+/// Turn right ascension and declination
+/// into azimuth and altitude.
+fn get_position(star: &Star, position: &Position) -> Option<PolarPosition> {
 
-    let p = StellarPosition { altitude: 1.0, azimuth: 1.0 };
+    match star.right_ascension {
+        Some(ra) => {
+            match star.declination {
+                Some(dec) => {
+                    // Borrow position
+                    let mut lat = position.latitude;
+                    let mut lon = position.longitude;
 
-    p
+                    // Calculate the hour angle
+                    let mut ha = 1.0_f64;
+
+
+                    // Calculate the azimuthal angle
+                    let x = -lat.sin()*dec.cos()*ha.cos() + lat.cos()*dec.sin();
+                    let y = dec.cos()*ha.sin();
+                    let mut azimuth = -y.atan2(x);
+
+                    // Calculate the altitude (zenith) angle
+                    let sin_alt = lat.sin()*dec.sin() + lat.cos()*dec.cos()*ha.cos();
+                    let altitude = sin_alt.asin();
+
+                    // Project stereographically
+                    let radius = altitude.sin() / (1.0_f64 - altitude.cos());
+
+                    Some(PolarPosition{
+                        r: radius,
+                        theta: azimuth
+                    })
+                },
+                None => {
+                    println!("No DEC was found.");
+                    return None;
+                },
+            }
+        },
+        None => {
+            println!("No RA was found.");
+            return None;
+        },
+    }
 }
 
-fn parse_data() -> Result<(), Box<Error>> {
+/// Parses stellar catalogue to deserialize
+/// data into Star structs.
+fn parse_data() -> Result<Vec<Star>, Box<Error>> {
+
+    let mut stars = Vec::new();
+
     let data_file_path = "data/hygdata_v3.csv";
-    let mut rdr = csv::Reader::from_path(data_file_path)?;
-    for result in rdr.deserialize() {
+    let mut reader = csv::Reader::from_path(data_file_path)?;
+    for result in reader.deserialize() {
         let record: Star = result?;
-        println!("{:?}", record);
+        stars.push(record);
     }
-    Ok(())
+
+    Ok(stars)
 }
 
 fn main() {
-    if let Err(err) = parse_data() {
-        println!("{}", err);
-        process::exit(1);
+    let result = parse_data();
+    let position = Position {latitude: 90.0, longitude: 0.0};
+
+    match result {
+        Ok(stars) => {
+            for s in &stars {
+                let pos = get_position(s, &position);
+                match pos {
+                    Some(val) => println!("{}", val),
+                    None => {},
+                }
+            }
+        },
+        Err(_) => {
+            println!("Unable to parse data.");
+        },
     }
 }
